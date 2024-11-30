2933b97acb4745fd1e9bfe4a4b40a565bbd7f51f
mallika214 <mallika.agarwal@capgemini.com>
2024-11-30 22:47:05 +0530

    Update app.js

--- challenge/app.js
+++ challenge/app.js
@@ -1,33 +1,5 @@
-/*const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
- 
-const productRoutes = require('./routes/productRoutes');
-//const authRoutes = require('./routes/authRoutes');
-const path = require('path');
-const authRoutes = require(path.resolve(__dirname, 'routes', 'authRoutes'));
 
- 
-// Middleware
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
- 
-// View Engine
-app.set('view engine', 'ejs');
- 
-// Routes
-app.use('/products', productRoutes);
-app.use('/auth', authRoutes);
- 
-// Root route
-app.get('/', (req, res) => {
-res.sendFile(__dirname + '/public/index.html');
-});
- 
-// Start server
-app.listen(3000, () => {
-console.log('Server running on http://localhost:3000');
-});*/
+
 
 const express = require('express');
 const bodyParser = require('body-parser');
